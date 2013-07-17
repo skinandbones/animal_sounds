0098786c59d6bc4ad51b19aa82dac9037f0f1e3d
Evgeny Rahman <evgeny.rahman@staples.com>
2013-07-17 18:50:50 -0400

    Now the rooster is going to make the correct sound

--- lib/animal_sounds.rb
+++ lib/animal_sounds.rb
@@ -3,16 +3,16 @@ require "animal_sounds/version"
 module AnimalSounds
 
   ANIMAL_SOUNDS = Hash.new('nothing').merge({
-  	dog: 'woof',
-  	cat: 'meow',
-  	rooster: 'Coo-caw!',
-  	chicken: 'cluck',
-  	duck: 'quack',
-  	cow: 'moo',
-  	sheep: 'baa'
+    dog: 'woof',
+    cat: 'meow',
+    rooster: 'caaawwww',
+    chicken: 'cluck',
+    duck: 'quack',
+    cow: 'moo',
+    sheep: 'baa'
   })
 
   def self.animal_sound(animal_name)
-  	ANIMAL_SOUNDS[animal_name.to_sym]
+    ANIMAL_SOUNDS[animal_name.to_sym]
   end
 end
